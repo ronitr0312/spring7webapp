f14755ece7864256c291e471599b89bd0c4502ba
ronitr0312 <ronitrawade0312@outlook.com>
2026-04-17 22:18:50 +0530

    spring7: initial setup

---
+++ src/main/java/guru/springframework/spring7webapp/spring7webapp/Spring7webappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7webapp.spring7webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7webappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring7webapp/spring7webapp/Spring7webappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7webapp.spring7webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
